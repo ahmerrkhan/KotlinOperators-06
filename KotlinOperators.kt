f4452fba6d693312286f6b507769da433b2b35c3
Muhammad Ahmer Khan <70189037+ahmerrkhan@users.noreply.github.com>
2020-10-09 22:00:21 +0500

    operators
    basics

---
+++ KotlinOperators.kt
@@ -0,0 +1,16 @@
+
+
+fun main(args : Array<String>){
+
+    //Arithmetic Operators
+    var a: Int = 20
+    var b : Int = 5
+
+    println(a+b)
+    println(a-b)
+    println(a*b)
+    println(a/b)
+    println(a%b)
+
+    
+}
